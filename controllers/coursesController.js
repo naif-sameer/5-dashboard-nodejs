08588c9ebf8c431b995ebfea3f29fc60a074ab2a
Naif <nayef7138@gmail.com>
2022-02-27 09:10:51 +0300

    update courses

--- controllers/coursesController.js
+++ controllers/coursesController.js
@@ -1,7 +1,47 @@
-const coursesInfo = (req, res) => {
-  res.render('courses');
+const courseModels = require('../models/courseModels');
+
+// get
+const getCourses = async (req, res) => {
+  const courses = await courseModels.getCourses();
+  res.render('courses', { courses });
+};
+
+// add
+const addCourse = async (req, res) => {
+  try {
+    let result = await courseModels.addCourse(req.body);
+
+    res.redirect('/courses');
+  } catch (err) {
+    res.json(err.message);
+  }
+};
+
+// update
+const updateCourse = async (req, res) => {
+  try {
+    let result = await courseModels.updateCourse(req.body);
+
+    res.redirect('/courses');
+  } catch (err) {
+    res.json(err.message);
+  }
+};
+
+// delete
+const deleteCourse = async (req, res) => {
+  try {
+    let result = await courseModels.deleteCourse(req.body);
+
+    res.redirect('/courses');
+  } catch (err) {
+    res.json(err.message);
+  }
 };
 
 module.exports = {
-  coursesInfo,
+  getCourses,
+  addCourse,
+  updateCourse,
+  deleteCourse,
 };
