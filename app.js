cb5bcc679af67d7ae92b2b461539fb11549a6766
Naif <nayef7138@gmail.com>
2022-02-27 09:19:18 +0300

    edit app file

--- app.js
+++ app.js
@@ -1,22 +1,18 @@
-const cookieParser = require('cookie-parser');
 const express = require('express');
 const httpErrors = require('http-errors');
-const logger = require('morgan');
+const http = require('http');
 const path = require('path');
 
+const app = express();
 const indexRouter = require('./routes/index');
 
-const app = express();
+const port = process.env.PORT || '3000';
 
+app.set('port', port);
 app.set('views', path.join(__dirname, 'views'));
-// view engine setup
 app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(indexRouter);
@@ -26,15 +22,9 @@ app.use((req, res, next) => {
   next(httpErrors(404));
 });
 
-// error handler
-app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+const server = http.createServer(app);
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+server.listen(port);
 
-module.exports = app;
+console.log(`server run on http://localhost:${port}`);
+
